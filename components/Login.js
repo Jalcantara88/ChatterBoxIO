2e175d55f5e2ed93cdb622299835c4ae80ceb08e
Julio Alcantara <jalcantara88@gmail.com>
2021-06-02 15:45:16 -0700

    styling login in page 4

--- components/Login.js
+++ components/Login.js
@@ -14,13 +14,20 @@ export default function Login(props) {
             <meta charSet="UTF-8"></meta>
           </Head>
 
-          <div className=" col-12 col-md-10 col-lg-6 mx-auto mt-5">
+          <div className=" col-12 col-md-10 col-lg-6 mx-auto mt-3">
             <h3 className="text-center rounded-top text-white m-0 py-2" style={{backgroundColor: "#5eaaa8"}}>Welcome to Chatter Box IO</h3>
-            <p className="text-center text-dark bg-white py-3 rounded-bottom">
+            <p className="text-center text-dark bg-white p-3 rounded-bottom">
               Set your username and Connect to Create or Join available Rooms
               <br/>
               Chat with other users, share ideas, or just observe.
               <br/>
+              <strong style={{backgroundColor: "#ff8474"}}>Features</strong>
+              <br/>
+              Usernames
+              <br/>
+              Rooms
+              <br/>
+              Emojis
               <br/>
               All are welcome!
             </p>
@@ -54,29 +61,28 @@ export default function Login(props) {
           
           <div className=" col-12 col-md-10 col-lg-6 mx-auto mt-5">
             <h3 className="text-center rounded-top text-white m-0" style={{backgroundColor: "#5eaaa8"}}>About This Project</h3>
-            <p className="m-0 text-center bg-white text-dark">
-              This was a project built for a Mintbean Hackathon: Create A Chat App
+            <p className="m-0 text-center bg-white text-dark p-3 rounded-bottom">
+              This was a project built for a Mintbean Hackathon: Create A Chat App by me <strong>JULIO ALCANTARA</strong>
               <br/>
               <br/>
-              <strong>Challenge</strong>
+              <strong style={{backgroundColor: "#ff8474"}}>Challenge</strong>
               <br/>
               Make a Chat Room app using React Next, Express and Socket.IO. Deploy to live site.
               <br/>
               <br/>
-              <strong>My Hurdles</strong>
+              <strong style={{backgroundColor: "#ff8474"}}>My Hurdles</strong>
               <br/>
               I have never worked with Socket.IO, Express, or set up a server to handle api requests. So for me, this was a very difficult challenge.
               I came accross issues mostly in getting my server hosted on heroku to accept incoming connections. Everything worked locally but not in deployment.
-              Figuring out how to make my own custom emoji buttons and figuring out how to constantly scroll the chat window down everytime a new message is sent was
+              <br/>
+              Also, Figuring out how to make my own custom emoji buttons and figuring out how to constantly scroll the chat window down everytime a new message is sent was
               also a little tricky to understand.
-              <br/>
-              <br/>
             </p>
   
           </div>
 
-          <div class="container-fluid bg-orange pt-1">
-            <p class="text-center text-dark m-0 p-3 pb-2 about-font">
+          <div class="container-fluid pt-1" style={{backgroundColor: "#ff8474"}}>
+            <p class="text-center m-0 p-3 pb-2 about-font">
               <strong class="text-white">MY PERSONAL LINKS</strong><br/>come check me out at my social accounts and send me some love!
               
             
